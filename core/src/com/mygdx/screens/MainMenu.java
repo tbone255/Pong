c1f1cef394297a53d309741101be306dd1fffbfa
tbone255 <taha.amir13971@gmail.com>
2016-06-17 18:51:07 -0400

    menu wat

--- core/src/com/mygdx/screens/MainMenu.java
+++ core/src/com/mygdx/screens/MainMenu.java
@@ -2,6 +2,7 @@ package com.mygdx.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -9,6 +10,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
 public class MainMenu implements Screen {
 	
@@ -16,19 +18,46 @@ public class MainMenu implements Screen {
 	private TextureAtlas atlas;
 	private Skin skin;
 	private Table table;
-	private TextButton buttonPlay, buttonExit;
+	private TextButton buttonPlay, buttonOption;
 	private BitmapFont white;
 	
 	private Label heading;
 	
 	@Override
 	public void show() {
+		atlas = new TextureAtlas("ui/blue/button.pack");
+		skin = new Skin(atlas);
+		
 		white = new BitmapFont(Gdx.files.internal("fonts/white.fnt"), false);
+		
+		table = new Table(skin);
+		table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.font = white;
+		textButtonStyle.up = skin.getDrawable("button.up.9.png");
+		textButtonStyle.down = skin.getDrawable("button.down.9.png");
+		textButtonStyle.pressedOffsetX = 1;
+		textButtonStyle.pressedOffsetY = -1;
+		
+		buttonPlay = new TextButton("PLAY", textButtonStyle);
+		buttonPlay.pad(20);
+		buttonOption = new TextButton("OPTIONS", textButtonStyle);
+		
+		table.add(buttonPlay);
+		table.debugActor();
+		stage.addActor(table);
+		
+		
+		
 	}
 
 	@Override
 	public void render(float delta) {
-		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_CLEAR_VALUE);
+		stage.act(delta);
+		stage.draw();
 	}
 
 	@Override
